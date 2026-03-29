832dc744a944028b7605c953d6b4b98001661d51
Gu35t09 <mattia.vallortigara@protonmail.com>
2026-03-29 08:33:51 -0400

    Added user-input-demo.py

---
+++ Python101/user-input-demo.py
@@ -0,0 +1,12 @@
+test = input("Enter the IP: ")
+print(test)
+
+print("---")
+
+while True:
+    test = input("Enter the IP: ")
+    print(">>> {}".format(test))
+    if test == "exit":
+        break
+    else:
+        print("exploiting...")
